4b425694c0a0b1b0bdabfe08cc96bb133182d910
Sam Larsen <sam@slarsen.io>
2024-09-13 02:39:02 -0400

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: false,
+	async headers() {
+		return [
+			{
+				source: "/(.*)",
+				headers: [
+					{
+						key: "Content-Security-Policy",
+						value: `default-src 'self'; img-src 'self' https: data:; media-src 'self' https: data:; script-src 'self' 'unsafe-inline' ${process.env.NODE_ENV === "development" && "'unsafe-eval'"} https: data:; frame-src 'self' https: data:; style-src 'self' 'unsafe-inline'; connect-src 'self' https://raw.githubusercontent.com https://gist.githubusercontent.com https://gitlab.com; font-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; upgrade-insecure-requests;`
+					},
+					{ key: "X-Content-Type-Options", value: "nosniff" },
+					{ key: "X-Frame-Options", value: "DENY" },
+					{ key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+					{
+						key: "Permissions-Policy",
+						value:
+							"geolocation=(), microphone=(), camera=(), payment=(), clipboard-read=(), accelerometer=(), gyroscope=(), magnetometer=(), usb=(), bluetooth=(), serial=(), sync-xhr=()"
+					},
+					{ key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+					{ key: "Cross-Origin-Resource-Policy", value: "same-origin" }
+				]
+			}
+		];
+	}
+};
+
+module.exports = nextConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}"
+	],
+	theme: {},
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: [
+			"light",
+			"dark",
+			"cupcake",
+			"bumblebee",
+			"emerald",
+			"corporate",
+			"synthwave",
+			"cyberpunk",
+			"retro",
+			"valentine",
+			"halloween",
+			"garden",
+			"forest",
+			"aqua",
+			"lofi",
+			"pastel",
+			"fantasy",
+			"wireframe",
+			"black",
+			"luxury",
+			"dracula",
+			"cmyk",
+			"autumn",
+			"business",
+			"acid",
+			"lemonade",
+			"night",
+			"coffee",
+			"winter",
+			"dim",
+			"nord",
+			"sunset"
+		]
+	}
+};
