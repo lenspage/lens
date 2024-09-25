73e657d3c21924c69602dc08206a419b6f9b8ae5
Sam Larsen <sam@slarsen.io>
2024-09-25 00:19:22 -0400

    Support attribute language field

--- src/components/ui/versions/v1/blocks/Code.js
+++ src/components/ui/versions/v1/blocks/Code.js
@@ -14,7 +14,14 @@ export default function Code({ item }) {
 	async function fetchCode() {
 		const response = await fetch(item.value);
 		const data = await response.text();
-		const highlight = hljs.highlightAuto(data).value;
+		let highlight;
+		if (item.attributes?.language) {
+			highlight = hljs.highlight(data, {
+				language: item.attributes.language
+			}).value;
+		} else {
+			highlight = hljs.highlightAuto(data).value;
+		}
 		const sanitized = DOMPurify.sanitize(highlight);
 		setCode(sanitized);
 		setCodeBlockLoaded(true);
